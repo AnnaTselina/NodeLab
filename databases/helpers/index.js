dffac6b58f186794c235ea1bc19b5ec289dfd7f8
Anna Celina <55737365+AnnaTselina@users.noreply.github.com>
2021-10-31 15:14:09 +0300

    refactor: getAllProducts helper

--- databases/helpers/index.js
+++ databases/helpers/index.js
@@ -1,6 +1,5 @@
 import Product from '../mongoDB/Models/Product.model.js';
 
 export const getAllProducts = async () => {
-    const res = await Product.find();
-    return res;
+    return await Product.find();
 }
