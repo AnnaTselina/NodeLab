f60ec54b3c344130930be859ac2d9488c8f94dec
Anna Celina <55737365+AnnaTselina@users.noreply.github.com>
2021-12-02 00:56:45 +0300

    feat: add indexes

--- src/DA/mongoDB/models/product.model.ts
+++ src/DA/mongoDB/models/product.model.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { prop, getModelForClass } from '@typegoose/typegoose';
 
 class ProductClass {
-  @prop({ required: true })
+  @prop({ required: true, index: { unique: true } })
   public displayName!: string;
 
   @prop({ required: true })
@@ -11,17 +11,36 @@ class ProductClass {
   @prop({ required: true })
   public categoryId!: mongoose.Schema.Types.ObjectId;
 
-  @prop({ required: true })
+  @prop({ required: true, index: true })
   public totalRating!: number;
 
-  @prop({ required: true })
+  @prop({ required: true, index: true })
   public price!: number;
 }
 
 const ProductModel = getModelForClass(ProductClass);
 
-//ProductModel.create({ displayName: 'GTA', createdAt: new Date(),  categoryId: "61843d4e4b7dfffb9e7439ff", totalRating: 5, price: 65});
-//ProductModel.create({ displayName: 'Cyberpank 2077', createdAt: new Date(),  categoryId: "61843d4e4b7dfffb9e7439ff", totalRating: 4, price: 60});
-//ProductModel.create({ displayName: 'Fifa', createdAt: new Date(),  categoryId: "61843c09f49e143f42495a08", totalRating: 3, price: 35});
+/*
+ProductModel.create({
+  displayName: 'GTA',
+  createdAt: new Date(),
+  categoryId: '61843d4e4b7dfffb9e7439ff',
+  totalRating: 5,
+  price: 65
+});
+ProductModel.create({
+  displayName: 'Cyberpank 2077',
+  createdAt: new Date(),
+  categoryId: '61843d4e4b7dfffb9e7439ff',
+  totalRating: 4,
+  price: 60
+});
+ProductModel.create({
+  displayName: 'Fifa',
+  createdAt: new Date(),
+  categoryId: '61843c09f49e143f42495a08',
+  totalRating: 3,
+  price: 35
+});*/
 
 export default ProductModel;
--- src/DA/postgresql/entities/product.entity.ts
+++ src/DA/postgresql/entities/product.entity.ts
@@ -1,20 +1,33 @@
-import { BaseEntity, Column, PrimaryGeneratedColumn, CreateDateColumn, Entity, ManyToMany, JoinTable } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  JoinTable,
+  Index
+} from 'typeorm';
 import { CategoryEntity } from './category.entity';
 
 @Entity('product')
 export class ProductEntity extends BaseEntity {
+  @Index({ unique: true })
   @PrimaryGeneratedColumn()
   _id?: number;
 
+  @Index({ unique: true })
   @Column()
   displayName!: string;
 
   @CreateDateColumn()
   createdAt!: Date;
 
+  @Index()
   @Column()
   totalRating!: number;
 
+  @Index()
   @Column()
   price!: number;
 
