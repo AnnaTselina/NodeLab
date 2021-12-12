bdf1f89af1c5c46596c3dec129c1175e53567350
Anna Celina <55737365+AnnaTselina@users.noreply.github.com>
2021-12-12 13:20:24 +0300

    feat: add products route params validation

---
+++ src/validators/validationSchemas.ts
@@ -0,0 +1,53 @@
+import { checkSchema } from 'express-validator';
+
+const PRICE_REGEX = /^((0|[1-9]\d*)(\.\d+)?)?:((0|[1-9]\d*)(\.\d+)?)?$/;
+const SORTBY_REGEX = /(displayName|price|totalRating|createdAt)+:(asc|desc)+/;
+
+export const validationProductsSchema = checkSchema(
+  {
+    displayName: {
+      optional: true
+    },
+    minRating: {
+      optional: true,
+      isFloat: {
+        options: { min: 0, max: 5 },
+        errorMessage: `Value for 'minRating' must be a floating point number between 0 and 5.`
+      }
+    },
+    price: {
+      optional: true,
+      matches: {
+        options: PRICE_REGEX,
+        errorMessage: `Value for 'price' must be in format 'x:y', where x and y are optional, positive (including 0), floating point numbers.`,
+        bail: true
+      },
+      custom: {
+        options: (value) => {
+          const priceRangesSplitted = value.split(':');
+          if (priceRangesSplitted[0] && priceRangesSplitted[1]) {
+            return priceRangesSplitted[0] <= priceRangesSplitted[1];
+          } else {
+            return true;
+          }
+        },
+        errorMessage: `Value for 'price' must be in format 'x:y', where x and y are optional and y >= x.`
+      }
+    },
+    sortBy: {
+      optional: true,
+      custom: {
+        options: (value) => SORTBY_REGEX.test(value),
+        errorMessage: `Value for 'sortBy' must be in format 'x:y', where x can be 'displayName', 'price', 'totalRating', 'createdAt', y - 'asc', 'desc'.`
+      }
+    },
+    page: {
+      optional: true,
+      isInt: {
+        options: { min: 1 },
+        errorMessage: 'Page must be full number higher from 0.'
+      }
+    }
+  },
+  ['params', 'query']
+);
