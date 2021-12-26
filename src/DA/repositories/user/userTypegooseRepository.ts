d2e67a97d5489c3f0e7d3cf3eb7deb1adde57cd7
Anna <annacelina@mail.ru>
2021-12-26 16:47:13 +0300

    feat: add user registration (mongo)

--- src/DA/repositories/user/userTypegooseRepository.ts
+++ src/DA/repositories/user/userTypegooseRepository.ts
@@ -1,12 +1,21 @@
 import { IUserRegistrationParams } from '../../../types/types';
+import { UserModel } from '../../mongoDB/models/user.model';
+import bcrypt from 'bcrypt';
 
 class UserTypegooseRepository {
   async createUser(userData: IUserRegistrationParams) {
-    return null;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+    const newUser = await UserModel.create({ ...userData, password: hashedPassword });
+    const result = await newUser.save();
+
+    return result ? result.username : null;
   }
 
   async getUserByUsername(username: string) {
-    return null;
+    const result = await UserModel.findOne({ username });
+    return result ? result : null;
   }
 }
 
