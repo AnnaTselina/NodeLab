759c9c9cac8c85457793f8b4ca200f00ef109656
Anna <annacelina@mail.ru>
2021-12-29 11:43:29 +0300

    feat: add UserRating entity

---
+++ src/DA/postgresql/entities/userRatings.entity.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('userRatings')
+export class UserRatings extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  _id?: number;
+
+  @Column()
+  userId!: number;
+
+  @Column()
+  productId!: number;
+
+  @Column()
+  rating!: number;
+}
