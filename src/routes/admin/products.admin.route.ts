6356eda088fb0586d1d834d683fd357cdd8b2611
Anna <annacelina@mail.ru>
2022-01-11 19:48:22 +0300

    feat: add getting product (admin)

--- src/routes/admin/products.admin.route.ts
+++ src/routes/admin/products.admin.route.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import HttpException from '../../exceptions/exceptions';
 import authenticateTokenMiddleware from '../../middlewares/authorization/authorization.middleware';
 import checkUserRoleMiddleware from '../../middlewares/checkUserRole/checkUserRole.middleware';
+import { ProductsService } from '../../service/products.service';
+
+const productService = new ProductsService();
 
 export const ProductsAdminRouter = (router: Router): void => {
   router.get(
@@ -9,6 +13,18 @@ export const ProductsAdminRouter = (router: Router): void => {
     (req, resp, next) => {
       checkUserRoleMiddleware(req, resp, next, 'admin');
     },
-    (req: Request, resp: Response, next: NextFunction) => {}
+    async (req: Request, resp: Response, next: NextFunction) => {
+      const { id } = req.params;
+      try {
+        const product = await productService.getProductById(id);
+        if (product) {
+          resp.status(200).json({ result: product });
+        } else {
+          throw new HttpException(404, `Product with id=${id} not found.`);
+        }
+      } catch (err) {
+        next(err);
+      }
+    }
   );
 };
